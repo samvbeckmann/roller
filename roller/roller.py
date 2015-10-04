512115c7422aea57a7a18d952eac542f9a6550da
SAM <samvbeckmann@gmail.com>
2015-10-04 15:45:38 -0500

    Specs 3-6 implemented.

--- roller/roller.py
+++ roller/roller.py
@@ -1,10 +1,35 @@
-import sys, re
+import sys
+import re
+
 line_num = 0
 file_line = ""
 with open(sys.argv[1], 'r') as lintee:
     for file_line in lintee:
         line_num += 1
-        if re.match("\s*$", file_line): continue
-        if re.match(".*[ \t]$", file_line): print("%d. Statement should not have trailing whitespace." % line_num)
-        if not re.match(".+[\{\};][\s]*$", file_line): print("%d. Statement should end with a semicolon." % line_num)
-    if not re.match(".*\n$", file_line): print("%d. File %s should end with a newline character." % (line_num, sys.argv[1]))
+        if re.match("\s*$", file_line):
+            continue
+        if re.search("[ \t]$", file_line):
+            print("%d. Statement should not have trailing whitespace." % line_num)
+
+        if not re.match(".+[\{\};][\s]*$", file_line):
+            print("%d. Statement should end with a semicolon." % line_num)
+
+        if re.match("\s*\{\s*", file_line):
+            print("%d. Open curly brace should not stand-alone." % line_num)
+        if re.search("\{.*\}", file_line):
+            print("%d. Closing curly brace should stand-alone." % line_num)
+
+        if re.search("(?<!=)==(?!=)", file_line):
+            print("%d. Should only use strict equality." % line_num)
+
+        regex = re.search("(\".*\")", file_line)
+        if regex is not None and not re.search("'", regex.string[regex.start():regex.end()]):
+            print("%d. Should use single quotes." % line_num)
+
+        if re.search(";.*;", file_line):
+            print("%d. Use only one statement per line." % line_num)
+        if len(file_line) > 80:
+            print("%d. Lines should not be longer than 80 characters." % line_num)
+
+    if not re.match(".*\n$", file_line):
+        print("%d. File %s should end with a newline character." % (line_num, sys.argv[1]))
--- test.js
+++ test.js
@@ -8,3 +8,16 @@ x = y + z
 def function (param) {
     doThing()
  }
+ 
+ def otherFunction (param2)
+ {
+	 x == doThing2();
+	 y === doThing2();
+ }
+
+"thing1";
+'thing2';
+"thing'3";
+
+thislskdfjhaslkdfhaslkdfjhaskldfhasdlkfjhasdkljfhaksldjfhaklsdjfhlaskdhfalskdjfhalskdjfh;
+var a; var b;
