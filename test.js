0fc74bab17427f38b0b55173d87f1cc579679f70
Sam <samvbeckmann@gmail.com>
2015-11-09 16:39:16 -0600

    Began rewriting tests.

--- test.js
+++ test.js
@@ -1,23 +1,6 @@
+// tests for spec one
 var x;
-var y  
-var z;  
-var z = functionCall() { }
-
-x = y + z
-
-def function (param) {
-    doThing()
- }
- 
- def otherFunction (param2)
- {
-	 x == doThing2();
-	 y === doThing2();
- }
-
-"thing1";
-'thing2';
-"thing'3";
-
-thislskdfjhaslkdfhaslkdfjhaskldfhasdlkfjhasdkljfhaksldjfhaklsdjfhlaskdhfalskdjfhalskdjfh;
-var a; var b;
+var y
+var x = function() {}
+// tests for spec two
+var z;
